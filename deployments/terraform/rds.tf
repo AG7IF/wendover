1f893e9f89fcaed53bdb8b624f1cf72b15fbf814
Robert Hawk <robert.hawk@gmail.com>
2024-03-27 21:45:46 -0600

    Fix?

--- deployments/terraform/rds.tf
+++ deployments/terraform/rds.tf
@@ -101,4 +101,7 @@ resource "aws_lambda_function" "migrate_wendover_db" {
   function_name   =   "wendover-migrate-db"
   image_uri       =   "712249788489.dkr.ecr.us-west-2.amazonaws.com/ag7if:wendsrv-latest"
   role            =   aws_iam_role.migrate_wendover_db.arn
+  image_config {
+    command = ["wendsrv migrate"]
+  }
 }
