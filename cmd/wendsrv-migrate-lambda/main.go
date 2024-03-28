52fd1d88de7c780331e3ada1f548ad1fcc8a7966
Robert Hawk <robert.hawk@gmail.com>
2024-03-28 13:07:06 -0600

    Trying building out entire migration Lambda

--- cmd/wendsrv-migrate-lambda/main.go
+++ cmd/wendsrv-migrate-lambda/main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
-	"context"
-
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
 
 	"github.com/ag7if/wendover/internal/aws"
+	"github.com/ag7if/wendover/internal/database"
 	_ "github.com/ag7if/wendover/internal/logging"
 )
 
-func handler(ctx context.Context) error {
-	panic("implement me!")
+func handler() error {
+	migration, err := database.NewMigration()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	version, err := migration.Up()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	log.Info().Uint("version", version).Msg("successfully migrated database")
+
+	return nil
 }
 
 func main() {
