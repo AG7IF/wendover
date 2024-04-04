e65d959e2fd6ba2668f204cb0c6133fb764b777f
Robert Hawk <robert.hawk@gmail.com>
2024-04-04 08:34:31 -0600

    Forgot ec2

--- deployments/terraform/ec2.tf
+++ deployments/terraform/ec2.tf
@@ -1,8 +1,11 @@
 resource "aws_instance" "wendover_a" {
   instance_type = "t2.micro"
   ami           = var.ami_id
-}
 
+  tags {
+    Name = "wendover-a"
+  }
+}
 
 /*
 resource "aws_eip" "wendover_a" {}
