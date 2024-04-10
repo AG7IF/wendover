93c7a08e479670843dfdc3b344e033bc5e2a8274
Robert Hawk <robert.hawk@gmail.com>
2024-04-09 19:41:33 -0600

    Added interface to EC2 instance

--- deployments/terraform/ec2.tf
+++ deployments/terraform/ec2.tf
@@ -3,10 +3,24 @@ resource "aws_key_pair" "wendover" {
   public_key  = var.public_key
 }
 
+resource "aws_network_interface" "wendover_a" {
+  ami       = var.ami_id
+  subnet_id = aws_subnet.wendover_a.id
+
+  tags = {
+    Name = "wendover-a"
+  }
+}
+
 resource "aws_instance" "wendover_a" {
   instance_type = "t2.micro"
   ami           = var.ami_id
   key_name      = aws_key_pair.wendover.key_name
+
+  network_interface {
+    network_interface_id = aws_network_interface.wendover_a.id
+    device_index         = 0
+  }
 
   tags = {
     Name = "wendover-a"
--- deployments/terraform/ecs.tf
+++ deployments/terraform/ecs.tf
@@ -120,9 +120,7 @@ resource "aws_ecs_service" "wendover_api" {
   network_configuration {
     subnets  = [
       aws_subnet.wendover_a.id,
-      aws_subnet.wendover_b.id,
-      aws_subnet.wendover_c.id,
-      aws_subnet.wendover_d.id
+      aws_subnet.wendover_b.id
     ]
     security_groups = [
       aws_security_group.wendover_api.id,
