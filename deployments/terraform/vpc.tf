1f6dab56bf8fe72bc99f4d6e60e08c50db360d85
Robert Hawk <robert.hawk@gmail.com>
2024-05-21 00:33:18 -0600

    Add allow all egress rule to SGs

--- deployments/terraform/vpc.tf
+++ deployments/terraform/vpc.tf
@@ -205,24 +205,11 @@ resource "aws_security_group" "wendover_api" {
   }
 
   egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    cidr_blocks       = ["0.0.0.0/0"]
+    ipv6_cidr_blocks  = ["::/0"]
   }
 }
 
@@ -238,9 +225,11 @@ resource "aws_security_group" "wendover_db" {
   }
 
   egress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    cidr_blocks       = ["0.0.0.0/0"]
+    ipv6_cidr_blocks  = ["::/0"]
   }
+
 }
